a529aa137699908194e8fdc36bbd28f784e3467d
ROVANA DIAS <rovana_dias@estudante.sesisenai.org.br>
2023-05-10 21:49:27 -0300

    aula dia 10/05

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -1,7 +1,49 @@
 import express from 'express';
+import bcrypt from 'bcrypt';
+import User from './../models/User.js';
+import jwt from 'jsonwebtoken';
 
 const login = express.Router();
 
-login.get('/', (req, res) => res.send("Rota de Login"));
+login.post('/', async (req, res) =>{
+    const { email, password } = req.body;
+
+    const registeredUser = await User.findOne(
+        { where: {email }}
+    ).catch(
+        (err) => {
+            console.log("Error: ", err);
+        }
+    )
+        if(!registeredUser) {
+            return res 
+            .status(400)
+            .json({message: "Email ou Senha Inválidos. "})
+        }
+        if (bcrypt.compareSync(password, registeredUser.password)) {
+            return res 
+            .status(400)
+            .json({message: "Email ou Senha Inválidos. "})
+    }
+    const token = jwt.sign(
+        {
+            id: registeredUser.id,
+            name: registeredUser.name,
+            admin: registeredUser.admin
+        },
+        process.env.JWT_SECRET,
+        
+        {
+            expiresIn: '1h'
+        }
+    );
+
+        res.json(
+            {
+                mesage: "BEM VINDO! ",
+                token: token
+            }
+        )
+});
 
 export default login;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,43 @@
-import express from 'express';
-
+import express, { response } from 'express';
+import User from '../models/User.js';
 const user = express.Router();
 
-user.get('/', function(req, res) {
+user.get('/', function (req, res) {
     res.send("Rota de Usuário")
 });
 
-user.get('/register', (req, res) => res.send("Cadastro de Novo Usuário"));
+user.post('/register', async (req, res) => {
+    const { name, email, password, admin } = req.body;
+
+    const alreadyExistsUser = await User.findOne(
+        { where: { email } }
+    ).catch((err) => console.log("Error: ", err));
+
+    if (alreadyExistsUser) {
+        console.log("Usuário Existente: " + alreadyExistsUser);
+        return res
+            .status(409)
+            .json({ message: "E-mail já utilizado por outro usuário." })
+
+    }
+    // pesquisa em casa sobre status http
+
+    const newUser = new User({ name, email, password, admin })
+
+    const savedUser = await newUser.save().catch((err) => {
+        console.log("Error: ", err);
+        res
+            .status(500)
+            .json({ error: "Não foi possível cadastrar o usuário." })
+
+    });
+
+    if (savedUser) {
+        console.log(savedUser);
+        res.json({ message: "Obrigado pelo seu Cadastro!" })
+    }
+}
+
+);
 
 export default user;
